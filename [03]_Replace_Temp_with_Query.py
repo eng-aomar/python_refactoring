f68fa77f329bcde2ccfa52ef63bc796a9cabe577
Alaa' Omar <alaa.omer2009@gmail.com>
2020-07-09 23:23:32 +0300

    Add files via upload

--- [03]_Replace_Temp_with_Query.py
+++ [03]_Replace_Temp_with_Query.py
@@ -1,3 +1,31 @@
 
+# --------------------------------------------------------------------------------------------------------------
+# ------------------------------------------Replace Temp with Query---------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
+# [Problem] : You place the result of an expression in a local variable for later use in your code.
+# [Solution] : Move the entire expression to a separate method and return the result from it. Query the method
+# instead of using a variable. Incorporate the new method in other methods, if necessary.
+# --------------------------------------------------------------------------------------------------------------
 
-Replace Temp with Query
+
+quantity = 10
+itemPrice = 5
+
+
+def calculateTotal():
+    basePrice = quantity * itemPrice
+    if basePrice > 1000:
+        return basePrice * 0.95
+    else:
+        return basePrice * 0.98
+
+
+def calculateTotal():
+    if basePrice() > 1000:
+        return basePrice() * 0.95
+    else:
+        return basePrice() * 0.98
+
+
+def basePrice():
+    return quantity * itemPrice
